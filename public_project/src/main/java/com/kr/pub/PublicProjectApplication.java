80afb3748c20e36e335f8e4656d312782aca90f0
hwch0 <hyewon0710@gmail.com>
2023-10-26 20:54:23 +0900

    '- hw: 프로젝트 생성

---
+++ public_project/src/main/java/com/kr/pub/PublicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kr.pub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicProjectApplication.class, args);
+	}
+
+}
---
+++ public_project/src/test/java/com/kr/pub/PublicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kr.pub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

70b73578935fa7d666c8ad70a12e721134e0a29c
hwch0 <hyewon0710@gmail.com>
2023-10-27 10:07:56 +0900

    - map-underscore-to-camel-case 테스트, jstl 버전 수정

---
+++ public_project/src/main/java/com/kr/pub/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.kr.pub.controller;
+
+import java.net.http.HttpResponse;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kr.pub.dto.UserDTO;
+import com.kr.pub.service.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/list")
+	public String userList(Model model) {
+		List<UserDTO> result = userService.getUserList();
+		System.out.println(result);
+		model.addAttribute("result",  result);
+		return "/user/userList";
+	}
+	
+	@GetMapping("/{userId}")
+	public String userInfo(Model model, @PathVariable String userId) {
+//		UserDTO user = UserDTO.builder()
+//				.userId(userId)
+//				.build();
+//		UserDTO result = userService.getUser(user);
+		UserDTO result = userService.getUser(userId);
+		model.addAttribute("result", result);
+		return "/user/userList";
+	}
+
+}

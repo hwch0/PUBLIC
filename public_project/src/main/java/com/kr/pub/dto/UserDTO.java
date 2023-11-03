dbcc01063d163ab1418f3daf052637e29ddb6323
hwch0 <hyewon0710@gmail.com>
2023-11-03 15:57:46 +0900

    [관리자] 메뉴 이미지 업로드 진행 중

--- public_project/src/main/java/com/kr/pub/dto/UserDTO.java
+++ public_project/src/main/java/com/kr/pub/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.kr.pub.dto;
 
 
+import java.time.ZonedDateTime;
 import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,18 +13,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
-	private String userId;
-	private String password;
-	private String uname;
-	private String birthdate;
-	private String phonenumber;
-	private String address;
-	private String email;
-	private int loginFailure;
-	private Date loginTime;
-	private Date logoutTime;
-	private Date regDate;
-	private int remainingTime;
-	private String roleId;
-	
+   private String userId;
+   private String password;
+   private String uname;
+   private String birthdate;
+   private String phonenumber;
+   private String address;
+   private String email;
+   private int loginFailure;
+   private ZonedDateTime loginTime;
+   private ZonedDateTime logoutTime;
+   private Date regDate;
+   private int remainingTime;
+   private String roleId;
+   
 }
